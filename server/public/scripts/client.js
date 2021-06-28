a4f6c570db490d29e6d13bc402f19bd223fc2009
Abram Chavez <abram.chavez820@gmail.com>
2021-06-28 13:40:45 -0500

    created ajax url /jokes then and err

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,24 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('DOM ready');
+
+
+//creating ajax request 
+$.ajax({
+    method:'POST',
+    url: '/jokes',
+    // req.body
+    data: {
+        whoseJoke: $('#whoseJokeIn').val(),
+        jokeQuestion: $('#questionIn').val(),
+        punchLine: $('#punchlineIn').val(),
+    }
+})
+.then(res => {
+    console.log('POST res', res);
+    fetchjokes();
+})
+.catch(err => {
+    console.log('POST failed', err);
+})
 }
